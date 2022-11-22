10efdbd20dbec9ed44a2bedc385cb380fbcc0168
pahlavon <pahlavon@chowis.com>
2022-11-22 16:04:43 +0500

    finished

--- src/main/kotlin/Problem_2469.kt
+++ src/main/kotlin/Problem_2469.kt
@@ -1,9 +1,9 @@
 fun main(args: Array<String>){
-   convertTemperature(36.50)
+   print(convertTemperature(36.50))
 }
-fun convertTemperature(celsius: Double): DoubleArray {
-    val celsius = readLine()!!.toDouble()
-    var Kelvin = String.format("%.5f",celsius + 273.15).toDouble()
-    var Fahrenheit = String.format("%.5f", celsius *1.80+ 32.00).toDouble()
-    return doubleArrayOf(Kelvin, Fahrenheit)
+fun convertTemperature(celsius: Double): List<String> {
+    var Kelvin = String.format("%.5f",celsius + 273.15)
+    var Fahrenheit = String.format("%.5f", celsius *1.80+ 32.00)
+
+    return  listOf(Kelvin,Fahrenheit)
 }
